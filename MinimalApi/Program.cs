e896e92952fb30ed89475746f4084035034e30ab
Prosthius <ietvntpd@duck.com>
2022-08-08 16:23:06 +1000

    Improved formatting, commented out code to match example: https://docs.microsoft.com/en-us/learn/modules/build-web-api-minimal-database/3-exercise-add-entity-framework-core

--- MinimalApi/Program.cs
+++ MinimalApi/Program.cs
@@ -1,16 +1,18 @@
 using Microsoft.OpenApi.Models;
-using PizzaStore.DB;
+// using PizzaStore.DB;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "PizzaStore API", Description = "Making the Pizzas you love", Version = "v1" });
+    c.SwaggerDoc("v1", new OpenApiInfo { 
+        Title = "PizzaStore API", 
+        Description = "Making the Pizzas you love", 
+        Version = "v1" });
 });
 
 var app = builder.Build();
-
 app.UseSwagger();
 app.UseSwaggerUI(c =>
 {
@@ -19,10 +21,12 @@ app.UseSwaggerUI(c =>
 
 app.MapGet("/", () => "Hello World!");
 
+/*
 app.MapGet("/pizzas/{id}", (int id) => PizzaDB.GetPizza(id));
 app.MapGet("/pizzas", () => PizzaDB.GetPizzas());
 app.MapPost("/pizzas", (Pizza pizza) => PizzaDB.CreatePizza(pizza));
 app.MapPut("/pizzas", (Pizza pizza) => PizzaDB.UpdatePizza(pizza));
 app.MapDelete("/pizzas/{id}", (int id) => PizzaDB.RemovePizza(id));
+*/
 
 app.Run();
