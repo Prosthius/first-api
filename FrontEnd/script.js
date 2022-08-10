12d99e2d79e843f705fc1b0a01417e34d2600833
Prosthius <ietvntpd@duck.com>
2022-08-10 12:40:08 +1000

    Added comments, successful GET request pizza/{id}

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,13 +1,28 @@
-async function fetchData() {
+// GET pizza
+async function fetchPizzas() {
     let response = await fetch('http://localhost:5136/pizzas');
     return await response.json();
 }
 
-async function renderData() {
-    let pizzas = await fetchData();
+async function renderPizzas() {
+    let pizzas = await fetchPizzas();
     pizzas.forEach(pizza => {
         console.log(pizza.id);
         console.log(pizza.name);
         console.log(pizza.description);
     });
+}
+
+// GET pizzas/{id}
+async function fetchPizza(id) {
+    id = 1;
+    let response = await fetch(`http://localhost:5136/pizza/${id}`);
+    return await response.json();
+}
+
+async function renderPizza() {
+    let pizza = await fetchPizza();
+    console.log(pizza.id);
+    console.log(pizza.name);
+    console.log(pizza.description);
 }
